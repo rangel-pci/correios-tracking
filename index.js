aed6488a3df6218f56d183782fdc54e3beaf3813
rangel-pci <rangelpereiracarneiro@gmail.com>
2020-11-29 23:20:22 -0300

    add Procfile

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 
-const trackingCorreios = require('tracking-correios');
 const correios = require('correios.js');
 
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
 	const {tracking} = req.query;
-
-	// trackingCorreios.track(tracking)
-	// .then((result) => {
-	// 	res.json({ message: 'ok', tracking, result });
-	// })
-	// .catch(() => {
-	// 	res.json({ message: 'error', error });
-	// })
 
 	correios.track(tracking)
 	.then((result) => {
@@ -30,4 +21,4 @@ app.get('/', (req, res) => {
 
 });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port http://localhost:${PORT}`));
